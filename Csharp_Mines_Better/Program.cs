6e3087b35afd5b91a22e2f3923e196ea3dcea980
JJ Ryan <jjryan111@yahoo.com>
2019-02-04 20:52:01 -0500

    Add project files.

---
+++ Csharp_Mines_Better/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Mines_Better
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
